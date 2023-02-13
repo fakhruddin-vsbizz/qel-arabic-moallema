6a52cbc4b714c6667c9cec315388426a231fc3ba
asus <someshanjana26@gmail.com>
2023-02-13 18:04:21 +0530

    added g-meet to db , announcement data to db and fetching of the announcement data for respective user

--- components/Teacher/Announcement.js
+++ components/Teacher/Announcement.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import CreateAnnouncement from '../Announcement/CreateAnnouncement'
-import ShowAnnouncement from '../Announcement/ShowAnnouncement'
+import React, { useState } from "react";
+import CreateAnnouncement from "../Announcement/CreateAnnouncement";
+import ShowAnnouncement from "../Announcement/ShowAnnouncement";
 
 const Announcement = () => {
+  const [updateState, setUpdateState] = useState(false);
+  const updateAnnouncement = () => {
+    setUpdateState((prev) => !prev);
+  };
   return (
     <>
-    <div className="">
-      <div className=" md:grid md:grid-cols-4 md:gap-6">
-        
-        <div className="mt-4 md:col-span-2 ">
-          {/* <form onSubmit={onBatchCreateHandler}> */}
-           
-            <CreateAnnouncement/>
-          {/* </form> */}
-        </div>
-        <div className=" md:col-span-2">
-        
-          <ShowAnnouncement/>
+      <div className="">
+        <div className=" md:grid md:grid-cols-4 md:gap-6">
+          <div className="mt-4 md:col-span-2 ">
+            {/* <form onSubmit={onBatchCreateHandler}> */}
+
+            <CreateAnnouncement update={updateAnnouncement} />
+            {/* </form> */}
+          </div>
+          <div className=" md:col-span-2">
+            <ShowAnnouncement clicked={updateState} />
+          </div>
         </div>
       </div>
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default Announcement
+export default Announcement;
